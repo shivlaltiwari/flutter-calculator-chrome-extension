fb402a71878478b7d0c7664acc0350088407b027
shiva tiwari <st172239@gmail.com>
2023-01-21 19:54:53 +0545

    bulid done

---
+++ android/app/src/main/kotlin/com/example/chorme/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chorme
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_simple_calculator/flutter_simple_calculator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter calculator"),
+        centerTitle: true,
+      ),
+      body: SimpleCalculator(
+        theme: CalculatorThemeData(
+          displayColor: Colors.black,
+          displayStyle: TextStyle(
+              fontSize: MediaQuery.of(context).size.width * 0.2,
+              color: Colors.yellow),
+          /*...*/
+        ),
+        value: 0.0,
+      ),
+    );
+  }
+}
